646793543cb46a538bc521d2c8e2e60f99e8fbe5
Nikita Shirshov <74493109+JayZMonster@users.noreply.github.com>
2022-04-17 09:38:53 +0500

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -8,10 +8,14 @@ class Notifier:
     def __init__(self, token, chat_id):
         self.token = token
         self.chat_id = chat_id
+        # Notify every user that bot is up
         for i in range(len(TG_TOKEN)):
             self.notify('Бот работает!', i)
 
     def notify(self, msg, num):
+        """
+        Sends message to every user via telegram api, it is required to specify which bot to use
+        """
         url = f"https://api.telegram.org/bot{self.token[num]}/sendMessage"
         for i in self.chat_id:
             msg_data = {
@@ -22,6 +26,9 @@ class Notifier:
             requests.post(url, data=msg_data)
 
     def work(self, info: List[dict]):
+        """
+        Generates a message for each summary and notifies about profitable
+        """
         for i in info:
             msg = f'Коридор (БЧ+Бинанс): RUB QIWI->{i["ticker"]["ticker"]}->USDT\n' \
                   f'[Bestchange]({i["link"]}): Отдаем {i["asset_price"]} RUB QIWI -> Получаем' \
@@ -31,6 +38,7 @@ class Notifier:
                   f'Кросс-курс: {i["cross_price"]}\n' \
                   f'Выгода: {round(i["profit"], 2)}%\n'
             print(msg)
+            # config has take-profit list
             if take_profit[0] < i['profit'] < take_profit[1] :
                 self.notify(msg, 0)
             if i['profit'] > take_profit[1]:
